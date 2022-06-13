1b395da7e45e006a1f0af19386291daceadd0a4d
Flor <mflorfernandez92@gmail.com>
2022-06-13 19:32:14 -0300

    Entrega 15/06

--- gastos_personales.sql
+++ gastos_personales.sql
@@ -8,72 +8,87 @@ DROP TABLE IF EXISTS tipodemoneda;
 
 -- Todos los create
 CREATE TABLE usuario (
-    id  INT AUTO_INCREMENT,
-    firstname VARCHAR(60),
-    lastname VARCHAR(60),
-          user VARCHAR(60),
-          password VARCHAR(60),
-          email VARCHAR (60),
+          id  INT AUTO_INCREMENT,
+          firstname VARCHAR(60) NOT NULL,
+          lastname VARCHAR(60) NOT NULL,
+          user VARCHAR(60) NOT NULL,
+          password VARCHAR(60) NOT NULL,
+          email VARCHAR (60) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE categoria (
-    id  INT AUTO_INCREMENT,
-    name VARCHAR(60),
+          id  INT AUTO_INCREMENT,
+          name VARCHAR(60) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE subcategoria (
-    id INT AUTO_INCREMENT,
-    id_category INT NOT NULL,
-    name VARCHAR(60),
+          id INT AUTO_INCREMENT ,
+          id_category INT NOT NULL,
+          name VARCHAR(60) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE cuentas (
           id INT AUTO_INCREMENT,
-    id_currency INT NOT NULL,
+          id_currency INT NOT NULL,
           id_user INT NOT NULL,
-    name VARCHAR(60),
-          description VARCHAR(100),
+          name VARCHAR(60) NOT NULL,
+          description VARCHAR(100) NOT NULL,
     PRIMARY KEY (id)
 );
 
 
 CREATE TABLE tipodemoneda (
           id INT AUTO_INCREMENT,
-    name VARCHAR(60),
-          short_name VARCHAR(3),
-          description VARCHAR (100), 
+          name VARCHAR(60) NOT NULL,
+          short_name VARCHAR(3) NOT NULL,
+          description VARCHAR (100) NOT NULL, 
     PRIMARY KEY (id)
 );
 
 CREATE TABLE transacciones (
           id INT AUTO_INCREMENT,
-    date DATETIME,
-          id_user INT,
-          id_category INT,
+          date DATETIME NOT NULL,
+          id_user INT NOT NULL,
+          id_category INT NOT NULL,
           id_subcategory INT,
-          id_acount INT,
-          detail VARCHAR (100),
-          amount DECIMAL,
+          id_account INT NOT NULL,
+          detail VARCHAR (100) NOT NULL,
+          amount DECIMAL NOT NULL,
     PRIMARY KEY (id)
 );
 
 
-
 -- Todos los FK
-ALTER TABLE subcategoria ADD CONSTRAINT fk_subcategoria_categoria FOREIGN KEY (id_category) REFERENCES categoria(id);
+ALTER TABLE subcategoria 
+    ADD CONSTRAINT fk_subcategoria_categoria 
+    FOREIGN KEY (id_category) REFERENCES categoria(id);
 
-ALTER TABLE cuentas ADD CONSTRAINT fk_tipodemoneda_cuentas FOREIGN KEY (id_currency) REFERENCES tipodemoneda (id);
-ALTER TABLE cuentas ADD CONSTRAINT fk_usuario_cuentas FOREIGN KEY (id_user) REFERENCES usuario (id);
+ALTER TABLE cuentas 
+    ADD CONSTRAINT fk_tipodemoneda_cuentas 
+    FOREIGN KEY (id_currency) REFERENCES tipodemoneda (id);
 
-ALTER TABLE transacciones ADD CONSTRAINT fk_usuario_transacciones FOREIGN KEY (id_user) REFERENCES usuario (id);
 
-ALTER TABLE transacciones ADD CONSTRAINT fk_categoria_transacciones FOREIGN KEY (id_category) REFERENCES categoria (id);
+ALTER TABLE cuentas 
+    ADD CONSTRAINT fk_usuario_cuentas 
+    FOREIGN KEY (id_user) REFERENCES usuario (id);
 
-ALTER TABLE transacciones ADD CONSTRAINT fk_subcategoria_transacciones FOREIGN KEY (id_subcategory) REFERENCES subcategoria (id);
+ALTER TABLE transacciones 
+    ADD CONSTRAINT fk_usuario_transacciones 
+    FOREIGN KEY (id_user) REFERENCES usuario (id);
 
-ALTER TABLE transacciones ADD CONSTRAINT fk_cuentas_transacciones FOREIGN KEY (id_acount) REFERENCES cuentas (id);
+ALTER TABLE transacciones 
+    ADD CONSTRAINT fk_categoria_transacciones 
+    FOREIGN KEY (id_category) REFERENCES categoria (id);
+
+ALTER TABLE transacciones 
+    ADD CONSTRAINT fk_subcategoria_transacciones 
+    FOREIGN KEY (id_subcategory) REFERENCES subcategoria (id);
+
+ALTER TABLE transacciones 
+    ADD CONSTRAINT fk_cuentas_transacciones
+    FOREIGN KEY (id_account) REFERENCES cuentas (id);
 
 
