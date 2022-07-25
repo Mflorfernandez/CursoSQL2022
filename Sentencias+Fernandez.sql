83bb1c3d89418735a039b3c0ac1de3e04077960e
Flor <mflorfernandez92@gmail.com>
2022-07-25 18:57:02 -0300

    DML

--- Sentencias+Fernandez.sql
+++ Sentencias+Fernandez.sql
@@ -1,9 +1,16 @@
 CREATE USER 'read_only_user'@'localhost' IDENTIFIED BY '2549MBJK';
+#Creo el usaurio 'read_only_user' que solamente tiene solo lectura en mi base de datos#
+
 
 CREATE USER 'read_and_write_user'@'localhost' IDENTIFIED BY 'SDFG1234';
+#Creo el usaurio 'read_and_write_user' que tiene lectura, insersion y modificacion en mi base de datos#
+
 
 GRANT SELECT ON gastos_personales.* TO 'read_only_user'@'localhost';
 SHOW GRANTS FOR 'read_only_user'@'localhost';
+#Le doy permisos de lectura solamente. No puede eliminar datos#
+
 
 GRANT SELECT, INSERT, UPDATE ON gastos_personales.* TO 'read_and_write_user'@'localhost';
 SHOW GRANTS FOR 'read_and_write_user'@'localhost';
+#Le doy permisos lectura, escritura y modificacion. No puede eliminar datos#
